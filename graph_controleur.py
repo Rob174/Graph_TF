ea757e53eea49d9e3ce58c84eb955a7c60b452e7
root <robin.128.456@gmail.com>
2020-04-21 01:10:09 +0200

    Modification gestion des mauvais modèles 2

--- graph_controleur.py
+++ graph_controleur.py
@@ -222,7 +222,7 @@ def create_model(hparam):
     global trop_param
     if trop_param == True:
         inpt = Input(shape=(256,256,3),dtype=tf.dtypes.float32,name='Entree_env10x256x256x3')
-        outpt = keras.layers.Subtract()([inpt,inpt])
+        outpt = tf.keras.layers.Subtract()([inpt,inpt])
         outpt = tf.keras.layers.GaussianNoise(5)(outpt)
         try:
             del model
